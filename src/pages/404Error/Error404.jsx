9f702a13c0fc555ca22ed625b58676068654aa01
Paco Menchaca <pacocoder86@outlook.com>
2022-07-21 19:08:13 -0500

    sing upok :fire:

--- src/pages/404Error/Error404.jsx
+++ src/pages/404Error/Error404.jsx
@@ -2,14 +2,18 @@ import Error from '../../assets/img/Error404.jpeg'
 import { useLocation, Link } from 'react-router-dom'
 import Header from '../../components/Header/Header'
 
+
 const Error404 = () => {
   const location = useLocation()
   return (
-    <><Header /><div>
-      <img src={Error} alt='' />
-      <p>No encontre lo que buscabas</p>
-      <p><Link to='/'>Regresa al Home</Link></p>
-    </div>
+    <><Header />
+      <div className='main-container'>
+        <img className='error-img' src={Error} alt='error' />
+      </div>
+      <div className='error-text'>
+        <p>Page Not Found</p>
+        <Link className='btn btn-dark' to='/'>Go Back</Link>
+      </div>
     </>
 
   )
